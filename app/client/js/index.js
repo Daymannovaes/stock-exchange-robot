8a605536658fe916fbfeec73463deb39d42f185d
Dayman Novaes <daymannovaes@gmail.com>
2015-11-05 11:00:11 -0200

    first chart working: ITUB4 from 2008

--- app/client/js/index.js
+++ app/client/js/index.js
@@ -1,5 +1,31 @@
 (function(undefined) {
 	"use strict";
 	
-	
+	$.get("public/candles_ibov_itub4_mini.csv", function(data) {
+		//$("#csv").html(data);
+		$('#container').highcharts({
+        data: {
+            csv: data,
+            dateFormat: "YYYY-mm-dd"
+        },
+        yAxis: {
+            title: {
+                text: 'Share prices'
+            }
+        },
+        plotOptions: {
+            series: {
+                marker: {
+                    enabled: false
+                }
+            }
+        },
+        title: {
+            text: 'Royal Mail shares over time'
+        },
+        subtitle: {
+            text: 'Auto-detection of <em>mm/dd/YYYY</em> dates in Highcharts Data module'
+        }
+    });
+	});
 })();
